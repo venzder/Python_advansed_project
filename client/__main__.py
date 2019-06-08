a554442c4fc2fe6a25060b6172d7728bc1322651
Sergey <sergivanov@list.ru>
2019-06-08 20:12:08 +0300

    Осуществил реализацию, проделанную на 4 занятии

--- client/__main__.py
+++ client/__main__.py
@@ -1,5 +1,7 @@
 import yaml
+import json
 import socket
+from datetime import datetime
 from argparse import ArgumentParser
 
 
@@ -26,8 +28,15 @@ try:
     sock = socket.socket()
     sock.connect((host, port))
     print('Client started')
+    action = input('Enter action ')
     data = input('Enter data: ')
-    sock.send(data.encode(encoding))
+    request = {
+        'action': action,
+        'data': data,
+        'time': datetime.now().timestamp()
+    }
+    s_request = json.dumps(request)
+    sock.send(s_request.encode(encoding))
     response = sock.recv(buffersize)
     print(response.decode(encoding))
 except KeyboardInterrupt:
